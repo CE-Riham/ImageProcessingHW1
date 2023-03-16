165ea09c0c12d41b615c1e5cebbc386cb47cfe00
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-03-16 02:27:41 +0200

    resize the image

--- HomeWork1/HomeWork1/HomeWork1.cpp
+++ HomeWork1/HomeWork1/HomeWork1.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 using namespace std;
 using namespace cv;
     
 int main()
 {
+    //to show the image in grayscale
     string path = "image.jpg";
     Mat image = imread(path, IMREAD_GRAYSCALE);
+
+    //resize the image (w, h)
+    resize(image, image, Size(600, 400));
     imshow("image", image);
     waitKey(0);
-    cout << "Hello World!\n";
+
+    return 0;
 }
